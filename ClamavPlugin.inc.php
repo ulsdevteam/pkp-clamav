42f898097697fd3e8fdbe3469358f2d948b9f53a
Clinton Graham <ctgraham@pitt.edu>
2020-04-20 10:40:42 -0400

    Further hotfox for #4

--- ClamavPlugin.inc.php
+++ ClamavPlugin.inc.php
@@ -33,13 +33,12 @@ class ClamavPlugin extends GenericPlugin {
 	 * 	the plugin will not be registered.
 	 */
 	function register($category, $path, $mainContextId = NULL) {
-		$success = parent::register($category, $path, $mainContextId);
 		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE'))
 			return true;
 		// Setting version information for backwards compatibility in other areas of the plugin
 		$versionDao = DAORegistry::getDAO('VersionDAO');
 		$this->currentAppVersion = $versionDao->getCurrentVersion();
-
+		$success = parent::register($category, $path, $mainContextId);
 		if ($success && $this->getEnabled()) {
 			// Enable Clam AV's preprocessing of uploaded files
 			HookRegistry::register('submissionfilesuploadform::validate', array($this, 'clamscanHandleUpload'));
